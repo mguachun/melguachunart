5b5882b4e4daff839520e94704f9a758169402d0
mguachun <1212malka@gmail.com>
2026-02-06 13:03:18 -0500

    fix: update about me again

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,8 @@ export default function About() {
   return (
     <div>
       <h1>Melissa Guachun is an award winning illustrator and fine artist based in Manhattan, New York. She graduated from CUNY Hunter College with a BFA in English, Language, and Composition. Her interests include reading horror, the muppets, and kickboxing.</h1>
-      <h3>Since 2013, Melissa has worked for: Don Giovanni Records, Starcleaner Records, Ecstatic Peace Library, Quail and Condor, The Bell House, The Screaming Females Fanclub, and more. Her work has been recognized by American Illustration, The School of Visual Arts, RAW Artists, and more.</h3>
+      <h3>Since 2013, Melissa has worked for: Don Giovanni Records, Starcleaner Records, Ecstatic Peace Library, Quail and Condor, The Bell House, The Screaming Females Fanclub, and more. Her work has been recognized by American Illustration, The School of Visual Arts, Brooklyn 
+      Vegan, RAW Artists, and more.</h3>
     </div>
   )
 }
